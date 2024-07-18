62f7d7a44fc7e010b866e6d9e3bedd3fdfbe1785
Fabian NP <burner.afire@gmail.com>
2024-07-18 01:47:45 -0600

    Advanced project, struchture, route, forms

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+// import { useState, useEffect } from "react";
+import { UserButton } from "@clerk/nextjs";
+
+
+
+export default function Home() {
+  // const [isClient, setIsClient] = useState(false)
+
+  // useEffect(() => {
+  //   setIsClient(true)
+  // }, []) 
+
+  return (
+    <div >
+      <h1 className="head-text text-left">Home</h1>
+    </div>
+  );
+}
